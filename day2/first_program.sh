930d0f282aceb7fe5a07ac3cd8882dc553b20afa
shivam96-bebni <shivambebni96@gmail.com>
2026-03-08 12:01:04 -0700

    user input and variable init program

---
+++ day2/first_program.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# this is first program of day2
+
+<< comment
+Anything
+written
+here will not be executed
+comment
+
+name="johnathan_cage"
+echo "hello i am $name!"
+
+# now taking user input 
+
+echo "what is your name"
+
+read name1
+
+echo "oh! i am $name1"
